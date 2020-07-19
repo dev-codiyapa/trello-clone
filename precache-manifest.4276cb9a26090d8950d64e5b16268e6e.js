a2180045c2c917eb1ec6740667aeb49c321f8224
AJMAC1032 <ajmac1032@AJMAC1098s-MacBook-Pro.local>
2020-07-19 17:46:54 +0530

    Updates

---
+++ precache-manifest.4276cb9a26090d8950d64e5b16268e6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805b2ad8f3852b9dd7ed264cd03ad336",
+    "url": "/trello-clone/index.html"
+  },
+  {
+    "revision": "be790bf4c597c6878a1e",
+    "url": "/trello-clone/static/css/main.d5ddfbe3.chunk.css"
+  },
+  {
+    "revision": "b6ae7bd84db920249ce9",
+    "url": "/trello-clone/static/js/2.53c52e9d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/trello-clone/static/js/2.53c52e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be790bf4c597c6878a1e",
+    "url": "/trello-clone/static/js/main.a29811c8.chunk.js"
+  },
+  {
+    "revision": "e86a24104ebac1f105b3",
+    "url": "/trello-clone/static/js/runtime-main.072ab029.js"
+  }
+]);
